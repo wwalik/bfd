5311532fe38260d38590a0f7f1d65a5956bb0fd3
w/malik <w.malik@pancakes.live>
2025-03-08 17:23:02 -0500

    added crude serialization

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include "get_line.cc"
 
 #define PROMPT ">>"
+#define LINE_SIZE_LIMIT 256
 #define DELIM " \n"
 int
 main(int argc, char *argv[])
@@ -14,18 +15,19 @@ main(int argc, char *argv[])
 	inter_ctx *inter = create_inter_ctx(256);
 	instr_set *instr = NULL;
 
-	char *line = malloc(256);
+	char *line = malloc(LINE_SIZE_LIMIT);
 	size_t bytes_read;
 	do
 	{
-		printf(">>");
+		printf(PROMPT);
 		bytes_read =
-			get_line(line, 256, stdin);
+			get_line(line, LINE_SIZE_LIMIT, stdin);
 
 		char *command = strtok(line, DELIM);
 		if (command == NULL)
 			continue;
 
+		// I need to fix this if statement chaos asap
 		if (strcmp(command, "file") == 0)
 		{
 			char *filename = strtok(NULL, DELIM);
@@ -38,11 +40,7 @@ main(int argc, char *argv[])
 		}
 		if (strcmp(command, "instructions") == 0)
 		{
-			// Show where we are in the instructions
-			if (instr == NULL)
-				printf("The instruction set is empty\n");
-			else 
-				printf("%s", instr->text);
+			show_instruction_set(instr);
 		}
 		if (strcmp(command, "memory") == 0)
 		{
@@ -50,20 +48,10 @@ main(int argc, char *argv[])
 		}
 		if (strcmp(command, "step") == 0)
 		{
-			if (instr == NULL)
-			{
-				printf("Please load an instruction set first\n");
-				continue;
-			}
 			step_inter_ctx(inter, instr);
 		}
 		if (strcmp(command, "run") == 0)
 		{
-			if (instr == NULL)
-			{
-				printf("Please load an instruction set first\n");
-				continue;
-			}
 			INTER_CTX_STATE inter_state;
 			while ( (inter_state = step_inter_ctx(inter, instr)) == INTER_SUCCESS);
 			switch (inter_state)
@@ -84,10 +72,45 @@ main(int argc, char *argv[])
 			destroy_inter_ctx(inter);
 			inter = create_inter_ctx(256);
 		}
+		if (strcmp(command, "save") == 0)
+		{
+			char *filename = strtok(NULL, DELIM);
+			if (filename == NULL) continue;
+
+			FILE *fp = fopen(filename, "w+");
+			if (fp == NULL)
+				perror("save");
+
+			fwrite(inter, sizeof(inter_ctx) + sizeof(TAPE_TYPE) * inter->tape_size, 1, fp);
+
+			fclose(fp);
+		}
+		if (strcmp(command, "load") == 0)
+		{
+			char *filename = strtok(NULL, DELIM);
+			if (filename == NULL) continue;
+
+			FILE *fp = fopen(filename, "r");
+			if (fp == NULL)
+				perror("load");
+
+			inter_ctx *old_inter = inter;
+			inter = unserialize_inter_ctx(fp);
+			if (inter == NULL)
+			{
+				printf("Could not load file\n");
+				inter = old_inter;
+			} else
+				destroy_inter_ctx(old_inter);
+
+			fclose(fp);
+		}
 
 	} while (bytes_read > 0);
+	
 	// CTRL+D i.e. EOF
-	if (bytes_read == 0) putchar('\n');
+	if (bytes_read == 0)
+		putchar('\n');
 
 	free(line);
 	destroy_inter_ctx(inter);
